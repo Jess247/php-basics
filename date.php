ad229f2ec7adc7c71a93c76a90294e602a74a80c
Jess247 <jesss8692@hotmail.com>
2021-05-23 14:59:57 +0200

    update dates
    current date, time and day of the week.

--- date.php
+++ date.php
@@ -7,14 +7,56 @@
     <title>Date</title>
 </head>
 <body>
-    <h1>These are the current dates</h1>
+    <?php
+        // current weekday
+        function weekday($wday){
+            switch($wday){
+                case 0:
+                    $day = "Sunday";
+                    break;
+                case 1:
+                    $day = "Monday";
+                    break;
+                case 2:
+                    $day = "Tuseday";
+                    break;
+                case 3:
+                    $day = "Wednesday";
+                    break;
+                case 4:
+                    $day = "Thursday";
+                    break;
+                case 5:
+                    $day = "Friday";
+                    break;
+                case 5:
+                    $day = "Saturday";
+                    break;
+            }
+            return $day;
+        }
+    ?>
+    <h1>Today</h1>
     <p>It is the 
         <?php
         // current date
         $timestamp = time();
         $date = date("d.m.Y", $timestamp);
         echo $date;
+        ?> 
+        and it is currently 
+        <?php
+        // current time
+        $date = date("H:i:s", $timestamp);
+        echo $date;
         ?>
+        o'clock.
+        I have the feeling today will be a beautiful 
+        <?php
+            $dayIndex = date("w", $timestamp);
+            print weekday($dayIndex)
+        ?>
+        .
     </p>
     
 </body>
