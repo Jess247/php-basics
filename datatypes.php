beeb60b40f7038baa331eaae17b09004e5f2df62
Jess247 <jesss8692@hotmail.com>
2021-05-24 14:02:38 +0200

    update datatype file

---
+++ datatypes.php
@@ -0,0 +1,13 @@
+<?php
+    $gameOver = false; // boolean - true or false
+    $score = 1234; // integer -  positiv or negativ number
+    $circleRatio = 3.14; // float 
+    $player = "Jess"; // String
+
+    // integers 
+    $dual = 0b110110; // decimal = 54
+    $decimal = 54;
+    $octal = 066; // decimal = 54
+    $hex = 0x36; // decimal = 54
+
+?>
