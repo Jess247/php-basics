fc9ffaf62f49a55ed9c0f9d31c9e0cf65ebfbd68
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-05-26 22:24:13 +0200

    update more operators file

---
+++ more-operators.php
@@ -0,0 +1,17 @@
+<?php 
+
+    // compare two numbers
+    $sum = 5 > 10; // false
+    var_dump($sum);
+    $sum = 5 < 10; // true 
+    var_dump($sum);
+
+    $num = 100; 
+    $sum = $num == 100;
+    var_dump($sum);
+    $sum = $num === "100";
+    var_dump($sum);
+
+
+
+?>
