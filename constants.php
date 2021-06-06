b45e6a847bb905d0b7309c05b1af3875e029f155
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-06-06 16:04:53 +0200

    update

--- constants.php
+++ constants.php
@@ -1,5 +1,5 @@
 <?php 
-    // define and use constats
+    // define and use constants
     const DIESEL = 1.38;
     define("PETROL", 1.55);
 
@@ -7,12 +7,12 @@
    echo "<p>$liter liter diesel const ".$liter * DIESEL ." Euro</p>";
    echo "<p>$liter liter petrol const ".$liter * PETROL ." Euro</p>";
 
-    // predefinded constants
+    // predefined constants
     echo "PHP-Version: ".PHP_VERSION . "<br/> \n";
     echo "Highest integer value: " .PHP_INT_MAX . "<br/> \n";
     echo "Pi: " .M_PI . "<br/> \n";
     echo "System: " .PHP_OS . "<br/> \n";
-    echo "list of all predefinde constants: https://www.php.net/manual/en/reserved.constants.php <br/> \n";
+    echo "list of all predefined constants: https://www.php.net/manual/en/reserved.constants.php <br/> \n";
 
     echo "The file name is : ". __file__ ;
     echo "The Line number is : ". __line__;
