0d9efe2a5a025e94f8f0e21563fb59a3c60b00a3
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-06-09 12:24:37 +0200

    update

---
+++ example2.php
@@ -0,0 +1,25 @@
+
+<?php 
+
+    $z1 = 10;
+    $z2 = 10;
+    $s1 = "10";
+
+    echo "results: ";
+
+    // $ergebnis = $z1 <= $z2 && $s1 == 10;
+    // var_dump($ergebnis);
+
+    // $ergebnis = !($z1 <= $z2) || $s1 === 10;
+    // var_dump($ergebnis);
+
+    // $ergebnis = $z1 === $z2 and $s1 > 10;
+    // var_dump($ergebnis);
+
+    $ergebnis = $z1 > $z2 || $s1 = 20;
+    var_dump($ergebnis);
+    var_dump($s1);
+
+    
+     ?> 
+    
