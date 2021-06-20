64f0ab4b62f6809cf2abd6adc2afa2473a1b82a9
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-06-20 16:22:09 +0200

    add arrays using loop

--- arrays.php
+++ arrays.php
@@ -6,14 +6,43 @@
     $arr[] = "Third element";
 
     var_dump($arr);
-    echo $arr[0];
     echo "<br/>";
 
     // other ways to declare or initialize an array
     $arrTwo = array("One", "Two", "Three");
     var_dump($arrTwo);
 
-    $arrThree = ["One", "Two", "Three"];
+    $arrThree = ["four", "five", "six"];
+
+    // access items form array
+    echo $arr[0];
+    echo "<br/>";
+
+    // add elements to array
+    $arrThree = [0 => "One",
+                 1 => "Two",
+                 2 => "Three"];
+    
+    var_dump($arrThree);
+
+    $arrThree[5] = "Four";
+
+    var_dump($arrThree);
+    echo "<br/>";
+
+    // add elements to an array using a loop
+    $evenNumbers = [];
+    $element = 0;
+
+    for($i = 1; $i <= 100; $i++){
+        if($i % 2 == 0){
+            $evenNumbers[$element] = $i;
+            $element++;
+        }
+    }
+    var_dump($evenNumbers);
+
+
 
 
 ?>
