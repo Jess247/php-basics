fdc5b87f80122232d191a688783ed96b94c1c6df
Jess247 <jesss8692@hotmail.com>
2021-06-20 15:20:49 +0200

    add more loops
    while, do while loops and examples of how to use break and continue.

--- loops.php
+++ loops.php
@@ -5,6 +5,37 @@ for($i = 1; $i <= 10; $i++) {
 }
 
 // do while loops
+$num = 0;
+const END = 20;
+do {
+    $num++;
+    echo $num;
+}while($num <= END)
 // while loop
+while($num < END) {
+    $num++;
+    echo $num;
+}
+// break 
+// will output "You are not a Teenager yet" and end the loop
+$age = 12;
+$flag = true;
+while($age <= 18) {
+    if($age <= 12 && $flag == true){
+        echo "You are not a Teenager yet!";
+        break;
+    }
+echo "You are a Teenager!";
+}
+
+// continue
+$year = 1999;
+while($year <= 2017) {
+    if(($year % 400) == 0 || (($year % 4) == 0 && ($year % 100) != 0)){
+        $year++;
+        continue;
+    }
+    echo "$year was no leap year";
+}
 
 ?>
