fbb8c80fe0c0131f6d4906caf80c2832c57cc50b
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-06-20 16:06:16 +0200

    declare arrays

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<?php
+    // How to declare an array
+    $arr = array();
+    $arr[] = "First element";
+    $arr[] = "Second element";
+    $arr[] = "Third element";
+
+    var_dump($arr);
+    echo $arr[0];
+    echo "<br/>";
+
+    // other ways to declare or initialize an array
+    $arrTwo = array("One", "Two", "Three");
+    var_dump($arrTwo);
+
+    $arrThree = ["One", "Two", "Three"];
+
+
+?>
