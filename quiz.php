4e01e28609eb1f6aefe712df873a0a3bb4281f70
Jess247 <jesss8692@hotmail.com>
2021-06-29 23:02:44 +0200

    update quiz questions

--- quiz.php
+++ quiz.php
@@ -1,2 +1,9 @@
 <?php
+// create a table with basic multiplications from 1 to 10
+
+// create a lottory generator (with out duplicates)
+
+// create a function that outputs the bigest integer
+
+// create a script which rearrages a string like ("Peterson, Mike" to "Mike, Peterson")
 ?>
