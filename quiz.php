b92fc8bad50163a662e1b8c0c07b9d6996c0dfb7
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-07-01 11:33:37 +0200

    update

--- quiz.php
+++ quiz.php
@@ -1,9 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplications</title>
+</head>
+<body>
+<h1>Basic multiplications</h1>
 <?php
-// create a table with basic multiplications from 1 to 10
-
-// create a lottory generator (with out duplicates)
-
-// create a function that outputs the bigest integer
-
-// create a script which rearrages a string like ("Peterson, Mike" to "Mike, Peterson")
+    echo "<table cellspacing = '10'>\n";
+    for($i = 1; $i <= 10; $i++){
+        if($i == 1) {
+            echo "<tr><th>*</th>";
+        }
+        echo "<th>$i</th>\t";
+        if($i == 10){
+            echo "</tr>";   
+        }
+        echo "<tr><th>$i</th></tr>";
+    }
 ?>
+    
+</body>
+</html>
