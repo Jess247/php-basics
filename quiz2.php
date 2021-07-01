bcdd8747df8f3e3c308dd0ca2fbd5b47e6d00c75
Jess247 <61388219+Jess247@users.noreply.github.com>
2021-07-01 11:48:44 +0200

    add quiz question 2

---
+++ quiz2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lotto</title>
+</head>
+<body>
+    <h1>Lottozahlen</h1>
+    <?php
+        for()
+        echo "Die gezogenen Zahlen sind: $zahlen";
+    ?>
+</body>
+</html>
