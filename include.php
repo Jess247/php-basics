643bae78bc717892a6424f9b63df97b08ad06f13
Jess247 <jesss8692@hotmail.com>
2021-07-16 14:50:06 +0200

    update include

--- include.php
+++ include.php
@@ -5,8 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Using include</title>
+    <?php
+        include("my_math_functions.inc.php");
+    ?>
 </head>
 <body>
-    
+    <?php
+    $sum = add(5,21);
+    echo $sum;
+    echo "<hr/>";
+
+    $sum = subtract($sum, 14);
+    echo $sum;
+    echo "<hr/>";
+
+    $sum = multiply($sum, 6);
+    echo $sum;
+    echo "<hr/>";
+
+    $sum = divide($sum,3);
+    echo $sum;
+    ?>
 </body>
 </html>
