e79effaebb11f45b4917dc6c5f907a2687c37066
Jess247 <jesss8692@hotmail.com>
2021-07-19 15:12:39 +0200

    update order form

--- order_form.php
+++ order_form.php
@@ -7,50 +7,72 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+if(isset($_POST['starter'])) {
+    $starter = $_POST['starter'];
+    $mainDish = $_POST['main'];
+    $desert = $_POST['dessert'];
+    $drink = $_POST['drinks'];
+    $guestStatus = $_POST['guestStatus'];
+} else {
+    $starter = "";
+    $mainDish = "";
+    $desert = "";
+    $drink = "";
+    $guestStatus = "";
+}
+?>
+    <h1>Menu</h1>
+    <h2>Place your order</h2>
     <form action="" method="post">
-
         <label for="starter">Starter: </label>
         <select id="starter" name="stater">
             <option value="">Choose a starter</option>
-            <option value="salad">Salad</option>
-            <option value="garlicBread">Garlic Bread</option>
-            <option value="bruschetta">Bruschetta</option>
-            <option value="antipasti">Antipasti</option>
+            <option value="<?php if($starter == 'salat')?>{echo 'selected'}">Salad</option>
+            <option value="<?php if($starter == 'garlicBread')?>{echo 'selected'}">Garlic Bread</option>
+            <option value="<?php if($starter == 'bruschetta')?>{echo 'selected'}">Bruschetta</option>
+            <option value="<?php if($starter == 'antipasti')?>{echo 'selected'}">Antipasti</option>
         </select>
 </br>
         <label for="main">Main Dish</label>
         <select id="main" name="main">
             <option value="">Choose a dish</option>
-            <option value="pizza">Pizza</option>
-            <option value="pasta">Pasta</option>
-            <option value="lasagne">Lasagne</option>
-            <option value="calzone">Calzone</option>
+            <option value="<?php if($mainDish == 'pizza')?>{echo 'selected'}">Pizza</option>
+            <option value="<?php if($mainDish == 'pasta')?>{echo 'selected'}">Pasta</option>
+            <option value="<?php if($mainDish == 'lasagne')?>{echo 'selected'}">Lasagne</option>
+            <option value="<?php if($mainDish == 'calzone')?>{echo 'selected'}">Calzone</option>
         </select>
 </br>
         <label for="dessert">Dessert</label>
         <select id="dessert" name="dessert">
             <option value="">Choose a dessert</option>
-            <option value="tiramisu">Tiramisu</option>
-            <option value="mouseAuChocolat">Mousse Au Chocolat</option>
-            <option value="iceCream">Ice Cream</option>
-            <option value="calzone">none</option>
+            <option value="<?php if($dessert == 'tiramisu')?>{echo 'selected'}">Tiramisu</option>
+            <option value="<?php if($dessert == 'mousse')?>{echo 'selected'}">Mousse Au Chocolat</option>
+            <option value="<?php if($dessert == 'iceCream')?>{echo 'selected'}">Ice Cream</option>
+            <option value="<?php if($dessert == 'none')?>{echo 'selected'}">none</option>
         </select>
 </br>
         <label for="drinks">Drinks</label>
         <select id="drinks" name="drinks">
             <option value="">Choose a dessert</option>
-            <option value="water">Water</option>
-            <option value="wine">Wine</option>
-            <option value="coke">Coke</option>
-            <option value="calzone">none</option>
+            <option value="<?php if($drinks == 'water')?>{echo 'selected'}">Water</option>
+            <option value="<?php if($drinks == 'wine')?>{echo 'selected'}">Wine</option>
+            <option value="<?php if($drinks == 'beer')?>{echo 'selected'}">Beer</option>
+            <option value="<?php if($drinks == 'none')?>{echo 'selected'}">none</option>
         </select>
 </br>
         <label for="employee">employee
-            <input type="radio" name="guestStatus" id="employee">
+            <input type="radio" name="guestStatus" id="employee" value="employee"<?php 
+            if($guestStatus != "guest") {
+                echo "checked";
+            }?>/>
         </label>
         </br>
         <label for="costumer">Costumer
-            <input type="radio" name="guestStatus" id="costumer">
+            <input type="radio" name="guestStatus" id="guest" value="guest"<?php 
+            if($guestStatus == "guest") {
+                echo "checked";
+            }?>/>
         </label>
         </br>
         <button type="submit">Order</button>
