1dd030eb80c14fcb97d0bfe33602d88df080a7d4
Jess247 <jesss8692@hotmail.com>
2021-07-20 13:58:02 +0200

    update form

--- order_form.php
+++ order_form.php
@@ -12,12 +12,12 @@ if(isset($_POST['starter'])) {
     $starter = $_POST['starter'];
     $mainDish = $_POST['main'];
     $desert = $_POST['dessert'];
-    $drink = $_POST['drinks'];
+    $drinks = $_POST['drinks'];
     $guestStatus = $_POST['guestStatus'];
 } else {
     $starter = "";
     $mainDish = "";
-    $desert = "";
+    $dessert = "";
     $drink = "";
     $guestStatus = "";
 }
@@ -28,37 +28,37 @@ if(isset($_POST['starter'])) {
         <label for="starter">Starter: </label>
         <select id="starter" name="stater">
             <option value="">Choose a starter</option>
-            <option value="<?php if($starter == 'salat')?>{echo 'selected'}">Salad</option>
-            <option value="<?php if($starter == 'garlicBread')?>{echo 'selected'}">Garlic Bread</option>
-            <option value="<?php if($starter == 'bruschetta')?>{echo 'selected'}">Bruschetta</option>
-            <option value="<?php if($starter == 'antipasti')?>{echo 'selected'}">Antipasti</option>
+            <option value="salat"<?php if($starter == 'salat'){echo 'selected';}?>>Salad</option>
+            <option value="garlicBread"<?php if($starter == 'garlicBread'){echo 'selected';}?>>Garlic Bread</option>
+            <option value="bruschetta"<?php if($starter == 'bruschetta'){echo 'selected';}?>>Bruschetta</option>
+            <option value="antipasti"<?php if($starter == 'antipasti'){echo 'selected';}?>>Antipasti</option>
         </select>
 </br>
         <label for="main">Main Dish</label>
         <select id="main" name="main">
             <option value="">Choose a dish</option>
-            <option value="<?php if($mainDish == 'pizza')?>{echo 'selected'}">Pizza</option>
-            <option value="<?php if($mainDish == 'pasta')?>{echo 'selected'}">Pasta</option>
-            <option value="<?php if($mainDish == 'lasagne')?>{echo 'selected'}">Lasagne</option>
-            <option value="<?php if($mainDish == 'calzone')?>{echo 'selected'}">Calzone</option>
+            <option value="pizza"<?php if($mainDish == 'pizza'){echo 'selected';}?>>Pizza</option>
+            <option value="pasta"<?php if($mainDish == 'pasta'){echo 'selected';}?>>Pasta</option>
+            <option value="lasagne"<?php if($mainDish == 'lasagne'){echo 'selected';}?>>Lasagne</option>
+            <option value="calzone"<?php if($mainDish == 'calzone'){echo 'selected';}?>>Calzone</option>
         </select>
 </br>
         <label for="dessert">Dessert</label>
         <select id="dessert" name="dessert">
             <option value="">Choose a dessert</option>
-            <option value="<?php if($dessert == 'tiramisu')?>{echo 'selected'}">Tiramisu</option>
-            <option value="<?php if($dessert == 'mousse')?>{echo 'selected'}">Mousse Au Chocolat</option>
-            <option value="<?php if($dessert == 'iceCream')?>{echo 'selected'}">Ice Cream</option>
-            <option value="<?php if($dessert == 'none')?>{echo 'selected'}">none</option>
+            <option value="tiramisu"<?php if($dessert == 'tiramisu'){echo 'selected';}?>>Tiramisu</option>
+            <option value="mousse"<?php if($dessert == 'mousse'){echo 'selected';}?>>Mousse Au Chocolat</option>
+            <option value="iceCream"<?php if($dessert == 'iceCream'){echo 'selected';}?>>Ice Cream</option>
+            <option value="none"<?php if($dessert == 'none'){echo 'selected';}?>>none</option>
         </select>
 </br>
         <label for="drinks">Drinks</label>
         <select id="drinks" name="drinks">
             <option value="">Choose a dessert</option>
-            <option value="<?php if($drinks == 'water')?>{echo 'selected'}">Water</option>
-            <option value="<?php if($drinks == 'wine')?>{echo 'selected'}">Wine</option>
-            <option value="<?php if($drinks == 'beer')?>{echo 'selected'}">Beer</option>
-            <option value="<?php if($drinks == 'none')?>{echo 'selected'}">none</option>
+            <option value="water"<?php if($drinks == 'water'){echo 'selected';}?>>Water</option>
+            <option value="wine"<?php if($drinks == 'wine'){echo 'selected';}?>>Wine</option>
+            <option value="beer"<?php if($drinks == 'beer'){echo 'selected';}?>>Beer</option>
+            <option value="none"<?php if($drinks == 'none'){echo 'selected';}?>>none</option>
         </select>
 </br>
         <label for="employee">employee
