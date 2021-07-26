5f532f2ea49043a36dd7cf42c6593652a50809b9
Jess247 <jesss8692@hotmail.com>
2021-07-26 08:34:22 +0200

    add select function

--- Form_practice/dynamic_selection.php
+++ Form_practice/dynamic_selection.php
@@ -5,29 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dynamic Selection</title>
+
+    <?php
+        function dynAuswahl($id, $name, $options, $multiple){
+            $ausgabe = "<select id='$id' name='$name'";
+            if($multiple){
+                $ausgabe .= "size =".count($options)." multiple>\n";
+            } else {
+                $ausgabe .= ">\n";
+            }
+            for($i = 0; $i < count($options); $i++){
+                $ausgabe .= "\t<option value='$options[$i]'>$options[$i]</option>\n";
+            }
+            if(isset($_POST["id"])){
+                if($_POST["options[$i]"]){
+                    $ausgabe .= "selected";
+                }
+            }
+            $ausgabe .= "</select>";
+            echo $ausgabe;
+        }
+    ?>
 </head>
 <body>
     <form action="post">
         <p>
-            <select name="one-option" id="dynamic">
-                <option value="Bread">Bread</option>
-                <option value="Butter">Butter</option>
-                <option value="Milk">Milk</option>
-                <option value="Eggs">Eggs</option>
-                <option value="Cheese">Cheese</option>
-                <option value="Peperoni">Peperoni</option>
-            </select>
+        <?php dynAuswahl("dynamisch1", "auswahl1", array("brot", "butter", "Kaese", "wurst"), false) ?>
         </p>
 
         <p>
-            <select name="multi-options" id="dynamicMulti" size="6" multiple>
-                <option value="Screws">Screws</option>
-                <option value="Nails">Nails</option>
-                <option value="Hammer">Hammer</option>
-                <option value="Screwdriver">Screwdriver</option>
-                <option value="Wood">Wooden plank</option>
-                <option value="Tarp">Tarp</option>
-            </select>
+        <?php dynAuswahl("dynamisch2", "auswahl2", array("fish", "haenchen", "rind", "schwein"), true) ?>
         </p>
         <p>
             <input type="submit" value="send">
