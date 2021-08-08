957361e5a3a23150dd9921b8b34c85cdd13e6d75
Jess247 <jesss8692@hotmail.com>
2021-08-08 18:20:28 +0200

    add class folder and file
    define class with objekts

---
+++ classes/car_class_ex.php
@@ -0,0 +1,62 @@
+<?php
+    // defining a class
+    class Carclass {
+        
+        // attribute
+        private $speed = 0;
+
+        // methods to initialize
+        public function initialize($standard) {
+            $this->$speed = $standard;
+        }
+
+        // break
+        public function brake($changes) {
+            if($this->$speed - $change < 0) {
+                $this->$speed = 0;
+            }
+            else {
+                $this->$speed - $change;
+            }
+        }
+
+        // accelerate
+        public function accelerate($change) {
+            $this->$speed = $this->$speed + $change;
+        }
+
+        // output current speed
+        public function output() {
+            echo "The current speed is ". $this->$speed .": ";
+        }
+    }
+
+    $car1 = new Carclass();
+    $car2 = new Carclass();
+
+    // call initialize function
+    $car1->initialize(0);
+    $car2->initialize(10);
+
+    echo "<hr/>";
+    echo "After initializing: ";
+    $car1->output();
+    $car2->output();
+    //call method accelerate
+    $car1->accelerate(20);
+    $car2->accelerate(100);
+    echo "<hr/>";
+    echo "After accelerating: ";
+    $car1->output();
+    $car2->output();
+    echo "<hr/>";
+    // call brake method
+    $car1->brake(10);
+    $car2->brake(50);
+    echo "<hr/>";
+    echo "After accelerating: ";
+    $car1->output();
+    $car2->output();
+    echo "<hr/>";
+
+?>
