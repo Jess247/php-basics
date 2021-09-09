c97fb7768d87c1282197b40d9e53c3e5d6bd336f
Jess247 <jesss8692@hotmail.com>
2021-09-09 17:09:55 +0200

    Update bankaccount.class.php

--- signup_Form/bankaccount/bankaccount.class.php
+++ signup_Form/bankaccount/bankaccount.class.php
@@ -1,4 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../dist/css/main.css">
+    <title>transaction</title>
+</head>
+<body>
+    
 <?php
 class Account {
     // Attribute
@@ -85,3 +95,5 @@ class Account {
      // instanzen
      $customer1 = new Account($number, $defaultBal, $name);
 ?>
+</body>
+</html>
